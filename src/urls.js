5cbf6d243b64a1c97bf556e2f999818e686558b8
nabeelktr <nabee446@gmail.com>
2023-10-02 17:45:54 +0530

    components added

--- src/urls.js
+++ src/urls.js
@@ -1,6 +1,6 @@
 import { API_KEY } from "./Component/Constants/constants";
 
-export const Originals = `/discover/tv?api_key=${API_KEY}&with_networks=213`
+export const Originals = `/discover/movie?api_key=${API_KEY}&with_genres=27`
 export const Action = `/discover/movie?api_key=${API_KEY}&with_genres=28`
 export const Romance = `/discover/movie?api_key=${API_KEY}&with_genres=10749`
 export const Documentries = `/discover/movie?api_key=${API_KEY}&with_genres=99`
